cf4fd47d7dc04cbbcfdd21116420535094af723c
Bob Zhang <zzt1981@126.com>
2019-11-19 09:16:46 +0800

    update

--- mybatis-demo/src/main/java/com/demo/sample/config/MybatisPlusConfig.java
+++ mybatis-demo/src/main/java/com/demo/sample/config/MybatisPlusConfig.java
@@ -1,5 +1,10 @@
 package com.demo.sample.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.demo.sample.mapper")
 public class MybatisPlusConfig {
 
 }
