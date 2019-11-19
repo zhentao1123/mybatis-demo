de15bad01549dd2eea3a14c6a731a2de0fc6fa35
Bob Zhang <zzt1981@126.com>
2019-11-19 11:32:03 +0800

    update

--- mybatis-demo/src/test/java/com/demo/ServiceTest.java
+++ mybatis-demo/src/test/java/com/demo/ServiceTest.java
@@ -1,5 +1,6 @@
 package com.demo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.junit.Test;
@@ -8,15 +9,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.demo.sample.model.entity.User;
 import com.demo.sample.service.IUserService;
 
+import lombok.extern.java.Log;
+
+@Log
 public class ServiceTest extends ApplicationTests {
 
 	@Autowired
 	IUserService service;
 	
 	@Test
-	public void test() {
+	public void list() {
 		List<User> userList = service.list();
-		System.out.println(userList.size());
 	}
 	
+	@Test
+	public void insert() {
+		List<User> userList = service.list();
+		log.info(userList.size()+"");
+		
+		User user = new User();
+		user.setAge(11);
+		user.setName("Mike");
+		user.setCreateTime(LocalDateTime.now());
+		service.save(user);
+		
+		userList = service.list();
+		log.info(userList.size()+"");
+	}
 }
