184525a652815e4bcf6d221fb301841b5b129206
Bob Zhang <zzt1981@126.com>
2019-12-10 17:38:12 +0800

    update

---
+++ mybatis-demo/src/main/java/com/demo/sample/config/WebMvcConfig.java
@@ -0,0 +1,80 @@
+package com.demo.sample.config;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport { 
+	
+	//@Resource(name="customObjectMapper")
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * 序列换成json时,将所有的long变成string
+         * 因为js中得数字类型不能包含所有的java long值
+         */
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+		return objectMapper;
+	}
+	
+	@Autowired
+	RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		//为swagger-ui设置资源映射
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 多个拦截器组成一个拦截器链
+        // addPathPatterns 用于添加拦截规则
+        // excludePathPatterns 用户排除拦截
+//		registry.addInterceptor(new APIAuthInterceptor())
+//				.addPathPatterns("/api/**")
+//				.excludePathPatterns("/login"); 
+		super.addInterceptors(registry);
+    }
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+	
+	@PostConstruct
+    public void init() {
+        List<HttpMessageConverter<?>> messageConverters = requestMappingHandlerAdapter.getMessageConverters();
+        for (HttpMessageConverter<?> messageConverter : messageConverters) {
+            if (messageConverter instanceof MappingJackson2HttpMessageConverter) {
+                MappingJackson2HttpMessageConverter m = (MappingJackson2HttpMessageConverter) messageConverter;
+                m.setObjectMapper(objectMapper());
+            }
+        }
+    }
+}
